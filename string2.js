233724c12971fa63acb4bccc3cc15c9eb8581f7b
Akanksha <akank612@gmail.com>
2022-03-11 12:28:54 +0530

    string2 added

---
+++ string2.js
@@ -0,0 +1,40 @@
+var arr= new Array();
+var part="";
+
+function string2(str)
+{
+    for(var i=0;i<str.length;i++)
+    {
+
+        if(str.charCodeAt(i)>=48 && str.charCodeAt(i)<=57 || str.charCodeAt(i)==46)
+       {
+
+
+        if(str.charAt(i)!='.')
+        {
+            part=part+str.charAt(i);
+        }
+
+        else
+        {
+            arr.push(Number(part));
+            part="";
+        }
+
+       if(i==str.length-1)
+       {
+        arr.push(Number(part));
+        return arr;
+
+       }
+
+
+    }//if close
+
+    else
+    return new Array();
+
+    }
+}
+
+module.exports.string2=string2;
