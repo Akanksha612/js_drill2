ebc303fd051bca77d302294a0065114bc4f0b147
Akanksha <akank612@gmail.com>
2022-03-11 12:30:40 +0530

    string4 added

---
+++ string4.js
@@ -0,0 +1,22 @@
+
+var str='';
+
+
+function string4(a)
+{
+
+for(var prop in a)
+{
+    
+
+    str = str+ a[prop].charAt(0).toUpperCase()+ a[prop].slice(1).toLowerCase()+" ";
+    
+   
+}
+
+return str;
+
+}
+
+
+module.exports.string4=string4;
