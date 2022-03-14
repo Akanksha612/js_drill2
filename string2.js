5a098bdb1bccde8dab5333169d5ea6bd4f6d640e
Akanksha <akank612@gmail.com>
2022-03-14 11:52:31 +0530

    string2 modified

--- string2.js
+++ string2.js
@@ -3,7 +3,52 @@ var part="";
 
 function string2(str)
 {
+   
     for(var i=0;i<str.length;i++)
+    {
+        if(str.charCodeAt(i)<48 || str.charCodeAt(i)>57)
+        {
+            if(str.charCodeAt(i)!=46)
+            {
+               return new Array();
+            }
+        }
+    }
+
+
+  return str.split(".");
+
+ }
+
+
+module.exports.string2=string2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* for(var i=0;i<str.length;i++)
     {
 
         if(str.charCodeAt(i)>=48 && str.charCodeAt(i)<=57 || str.charCodeAt(i)==46)
@@ -32,9 +77,4 @@ function string2(str)
     }//if close
 
     else
-    return new Array();
-
-    }
-}
-
-module.exports.string2=string2;
+    return new Array();  */
