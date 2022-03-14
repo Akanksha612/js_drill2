df05a00f524157694ad35970dd2bc13addfb963d
Akanksha <akank612@gmail.com>
2022-03-14 11:53:23 +0530

    string3 modified

--- string3.js
+++ string3.js
@@ -2,19 +2,52 @@ var part='';
 
 function string3(str)
 {
-    for(var i=0;i<str.length;i++)
-    {
-        if(str.charAt(i)=='/')
-        {
-            for(var j=i+1;str.charAt(j)!='/';j++)
-            {
- 
-                part=part+str.charAt(j);
-            }
 
-            return Number(part);
-        }
-    }
+    const arr= str.split("/");
+    return arr[1];
+   
 }
 
 module.exports.string3=string3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*for(var i=0;i<str.length;i++)
+{
+    if(str.charAt(i)=='/')
+    {
+        for(var j=i+1;str.charAt(j)!='/';j++)
+        {
+
+            part=part+str.charAt(j);
+        }
+
+        return Number(part);
+    }
+}  */
